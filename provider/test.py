613513437c1b7c1d8c7eb776b371620e053cd07a
Tim Preece <tdpreece@gmail.com>
2016-01-17 13:08:38 +0000

    Add todo note.

--- provider/test.py
+++ provider/test.py
@@ -6,6 +6,8 @@ class TestProvider(unittest.TestCase):
     def test_contract(self):
         contractual_response = requests.get('http://localhost:4545/an-item-slug')
         actual_response = requests.get('http://localhost:1912/an-item-slug')
+        # TODO Don't want to test that they're equal, just that the consumer
+        # can use what's provided.
         self.assertEqual(actual_response.status_code, contractual_response.status_code)
         self.assertEqual(actual_response.text, contractual_response.text)
 
