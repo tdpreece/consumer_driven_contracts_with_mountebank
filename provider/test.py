9b0844c68acc71f0a14560aabbed4bdea0859a74
Tim Preece <tdpreece@gmail.com>
2016-01-17 15:54:09 +0000

    Change test contract to expect a json response from the provider.

--- provider/test.py
+++ provider/test.py
@@ -6,10 +6,10 @@ class TestProvider(unittest.TestCase):
     def test_contract(self):
         contractual_response = requests.get('http://localhost:4545/an-item-slug')
         actual_response = requests.get('http://localhost:1912/an-item-slug')
-        # TODO Don't want to test that they're equal, just that the consumer
-        # can use what's provided.
+        contractual_response_json = contractual_response.json()
+        actual_response_json = actual_response.json()
         self.assertEqual(actual_response.status_code, contractual_response.status_code)
-        self.assertEqual(actual_response.text, contractual_response.text)
+        self.assertDictContainsSubset(contractual_response_json, actual_response_json)
 
 
 if __name__ == '__main__':
