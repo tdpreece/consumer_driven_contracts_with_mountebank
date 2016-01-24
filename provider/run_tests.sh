235f8bff6df9067af1778256a32fe5d07f375ac2
Tim Preece <tdpreece@gmail.com>
2016-01-24 13:09:01 +0000

    Give the servers longer to come up.

--- provider/run_tests.sh
+++ provider/run_tests.sh
@@ -5,14 +5,14 @@ set -e
 cd ../consumer_contracts_for_provider
 ./restart_mountebank &> /dev/null &
 mountebank_pid=$!
-sleep 1
+sleep 2
 echo "mountebank pid=$mountebank_pid"
 cd -
 
 cd provider
 python -m SimpleHTTPServer 1912 &> /dev/null &
 provider_pid=$!
-sleep 1
+sleep 2
 echo "provider_pid=$provider_pid"
 cd -
 
