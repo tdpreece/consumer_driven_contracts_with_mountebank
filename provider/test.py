0ad5ffd41f6f903737ad0d35bea946be797227c3
Tim Preece <tdpreece@gmail.com>
2016-01-24 13:42:12 +0000

    Write test and implementation for provider against consumer1.

--- provider/test.py
+++ provider/test.py
@@ -2,14 +2,22 @@ import requests
 import unittest
 
 
-class TestProvider(unittest.TestCase):
+class TestAgainstConsumer1(unittest.TestCase):
+    def setUp(self):
+        self.stub_host_port = 'http://localhost:4545'
+        self.actual_host_port = 'http://localhost:1912'
+
     def test_contract(self):
-        contractual_response = requests.get('http://localhost:4545/an-item-slug')
-        actual_response = requests.get('http://localhost:1912/an-item-slug')
+        path = '/record/100'
+        contractual_response = requests.get(self.stub_host_port+path)
+        actual_response = requests.get(self.actual_host_port+path)
+        self.assertEqual(actual_response.status_code, contractual_response.status_code)
         contractual_response_json = contractual_response.json()
         actual_response_json = actual_response.json()
-        self.assertEqual(actual_response.status_code, contractual_response.status_code)
-        self.assertDictContainsSubset(contractual_response_json, actual_response_json)
+        self.assertDictContainsSubset(
+            contractual_response_json,
+            actual_response_json
+        )
 
 
 if __name__ == '__main__':
