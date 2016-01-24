4e17d0d69a2b9dfcb9097db82de2f2825717b02c
Tim Preece <tdpreece@gmail.com>
2016-01-24 15:39:25 +0000

    Replace static files implementation of provider with python script.

---
+++ provider/provider.py
@@ -0,0 +1,31 @@
+import BaseHTTPServer
+import time
+
+
+# References:
+# https://wiki.python.org/moin/BaseHttpServer
+HOST_NAME = 'localhost'
+PORT_NUMBER = 1912
+
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        """Respond to a GET request."""
+        # If someone went to "http://something.somewhere.net/foo/bar/",
+        # then s.path equals "/foo/bar/".
+        s.send_response(200)
+        s.send_header("Content-type", "application/json")
+        s.end_headers()
+        s.wfile.write("{\"a\": 111, \"b\": 222}")
+
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
--- provider/run_tests.sh
+++ provider/run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 cd ../consumer_contracts_for_provider
 ./restart_mountebank &> /dev/null &
@@ -9,12 +10,10 @@ sleep 2
 echo "mountebank pid=$mountebank_pid"
 cd -
 
-cd provider
-python -m SimpleHTTPServer 1912 &> /dev/null &
+python provider.py &> /dev/null &
 provider_pid=$!
 sleep 2
 echo "provider_pid=$provider_pid"
-cd -
 
 python test.py
 
