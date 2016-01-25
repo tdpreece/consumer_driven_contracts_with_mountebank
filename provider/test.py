16e435eb44b64b24f2debc9ee78975102ad208e5
Tim Preece <tdpreece@gmail.com>
2016-01-25 20:32:28 +0000

    Add tests for provider against consumer2.

--- provider/test.py
+++ provider/test.py
@@ -34,5 +34,31 @@ class TestAgainstConsumer1(unittest.TestCase):
         provider.DataStore.delete_record(record)
 
 
+class TestAgainstConsumer2(unittest.TestCase):
+    def setUp(self):
+        self.stub_host_port = 'http://localhost:4546'
+        self.actual_host_port = 'http://localhost:1912'
+
+    def test_contract(self):
+        path = '/record/100'
+        record = {"id": 100, "a": 123, "b": 222, "c": 333}
+        provider.DataStore.save_record(record)
+
+        contractual_response = requests.get(self.stub_host_port+path)
+        actual_response = requests.get(self.actual_host_port+path)
+
+        self.assertEqual(
+            actual_response.status_code,
+            contractual_response.status_code
+        )
+        # The consumer shouldn't mind if the provider returns some
+        # extra data.
+        self.assertDictContainsSubset(
+            contractual_response.json(),
+            actual_response.json()
+        )
+        provider.DataStore.delete_record(record)
+
+
 if __name__ == '__main__':
     unittest.main()
